2623a72cc917093b07eaa64d80dfef1c3a4509eb
Addepalli <MadhuMalhar.Addepalli@Invesco.com>
2022-06-11 18:01:07 +0530

    Initial commit

---
+++ src/main/java/com/example/duplicateentityerror/DuplicateentityerrorApplication.java
@@ -0,0 +1,13 @@
+package com.example.duplicateentityerror;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuplicateentityerrorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuplicateentityerrorApplication.class, args);
+	}
+
+}
